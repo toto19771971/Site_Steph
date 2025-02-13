e3dca142833973c4fb61847f2c77ef7e69118d5a
Abdel MOURAK <a_mourak@yahoo.fr>
2025-02-13 09:02:24 +0100

    Correction du port pour Render

--- app_steph.py
+++ app_steph.py
@@ -43,5 +43,10 @@ def contact():
 def index():
     return render_template('index.html')
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True,port=5003)
+    port = int(os.environ.get("PORT", 10000))  # Render attribue automatiquement un port
+    app.run(host='0.0.0.0', port=port)
+
+
